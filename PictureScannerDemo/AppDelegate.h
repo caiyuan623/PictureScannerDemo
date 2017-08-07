ac25fe88ebeb7299830142ce0d27f93bed380d1f
caiyuan623 <15051987332@163.com>
2017-08-07 16:06:52 +0800

    Initial Commit

---
+++ PictureScannerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PictureScannerDemo
+//
+//  Created by lemo on 2017/8/7.
+//  Copyright © 2017年 MyCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PictureScannerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PictureScannerDemo
+//
+//  Created by lemo on 2017/8/7.
+//  Copyright © 2017年 MyCompany. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
